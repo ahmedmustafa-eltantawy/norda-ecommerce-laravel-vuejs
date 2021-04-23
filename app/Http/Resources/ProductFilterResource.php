d092127b8708562702a52622e8f2c49653c3393c
Ahmed Mostafa Eltantawy <ahmedmostafaui@gmail.com>
2021-04-23 18:07:58 +0200

    Product Filter Resource & Collection

---
+++ app/Http/Resources/ProductFilterResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductFilterResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'category_name' => $this->category_name,
+            'price' => $this->price,
+            'discounted_price' => $this->discounted_price,
+            'discount_percentage' => floor((1 - ($this->discounted_price / $this->price) ) * 100) . '%' ,
+            'description' => $this->description,
+            'colors' => explode(',', $this->colors),
+            'sizes' => explode(',', $this->sizes),
+            'stock_quantity' => $this->stock_q,
+        ];
+    }
+}
