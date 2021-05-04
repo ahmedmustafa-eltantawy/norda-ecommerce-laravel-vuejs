be5303ea2326e591b1e9ecc4e0686ddcd7dd105d
Ahmed Mostafa Eltantawy <ahmedmostafaui@gmail.com>
2021-05-04 11:09:07 +0200

    Added Shop Components + Single Porduct Component

--- resources/views/front/shop.blade.php
+++ resources/views/front/shop.blade.php
@@ -3,26 +3,7 @@
 @section('front')
 <div class="container">
     <div class="shop-area pt-120 pb-120">
-        <div class="container">
-            <div class="row flex-row-reverse">
-                <div class="col-lg-9">
-                    <div class="shop-bottom-area">
-                        <div class="tab-content jump">
-                            <div id="shop-1" class="tab-pane active">
-                                <div class="row">
-                                    <shop-single-product></shop-single-product>
-                                </div>
-                            </div>
-
-                        </div>
-                        <shop-pagination></shop-pagination>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <shop-sidebar></shop-sidebar>
-                </div>
-            </div>
-        </div>
+        <shop-page></shop-page>
     </div>
 </div>
 @endsection
--- resources/views/front/single-product.blade.php
+++ resources/views/front/single-product.blade.php
@@ -5,23 +5,25 @@
     <div class="container">
         <single-product></single-product>
     </div>
-    <div class="description-review-wrapper pb-110">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <single-product-reviews-list></single-product-reviews-list>
-                    <single-product-reviews-form></single-product-reviews-form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="related-product pb-115">
+
+    <div class="related-product pt-115 pb-115">
         <div class="container">
             <div class="section-title mb-45 text-center">
                 <h2>Related Product</h2>
             </div>
         </div>
         <single-product-related-by-category></single-product-related-by-category>
+    </div>
+
+    <div class="description-review-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <single-product-reviews-form></single-product-reviews-form>
+                    <single-product-reviews-list></single-product-reviews-list>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
